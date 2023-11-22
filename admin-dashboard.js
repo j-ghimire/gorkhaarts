5e6339b4951378e1b134c159409f582ecc3bc040
j-ghimire <148534160+j-ghimire@users.noreply.github.com>
2023-11-22 21:41:21 +0545

    Update admin-dashboard.js

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -1,13 +1,34 @@
-function postPhoto() {
-    console.log('Button clicked!'); // Add this line
-    const photo = document.getElementById('photo').value;
-    const description = document.getElementById('description').value;
 
-    // Simulate posting photo (replace with server-side logic)
-    if (photo && description) {
-        // Successful post, redirect to post-photo.html
-        window.location.href = 'post-photo.html';
-    } else {
-        alert('Please choose a photo and provide a description.');
-    }
-}
+function postPhoto() {
+    console.log('Button clicked!');
+    const photoInput = document.getElementById('photo');
+    const descriptionInput = document.getElementById('description');
+  
+    const photo = photoInput.files[0];
+    const description = descriptionInput.value;
+  
+    const formData = new FormData();
+    formData.append('photo', photo);
+    formData.append('description', description);
+  
+    fetch('https://api.github.com/repos/j-ghimire/gorkhaarts/contents/photos/' + photo.name, {
+      method: 'PUT',
+      headers: {
+        Authorization: 'Bearer YOUR_PERSONAL_ACCESS_TOKEN',
+      },
+      body: JSON.stringify({
+        message: 'Upload photo',
+        content: formData.get('photo'),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Photo uploaded successfully:', data);
+        // Redirect or update UI as needed
+      })
+      .catch((error) => {
+        console.error('Error uploading photo:', error);
+        alert('Error uploading photo. Please try again.');
+      });
+  }
+  
