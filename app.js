c3a459c3b02073cdfc748e39e2faf278c446ac57
j-ghimire <148534160+j-ghimire@users.noreply.github.com>
2023-11-22 22:41:49 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ function isAdmin() {
     return urlParams.has('admin');
 }
 
-window.onload = function () {
+const photosPerPage = 10; // Adjust the number of photos per page
+
+function loadPhotos(page) {
+    const startIndex = (page - 1) * photosPerPage;
+    const endIndex = startIndex + photosPerPage;
+
     fetch('https://api.github.com/repos/j-ghimire/gorkhaarts/contents/photos')
         .then((response) => response.json())
         .then((data) => {
             const photoContainer = document.getElementById('photo-container');
-            data.forEach((photo) => {
+            photoContainer.innerHTML = ''; // Clear existing photos
+
+            // Add photos to the container for the selected page
+            for (let i = startIndex; i < endIndex && i < data.length; i++) {
+                const photo = data[i];
                 const img = document.createElement('img');
                 img.src = photo.download_url;
                 img.alt = 'Uploaded photo';
-                img.style.width = '300px'; // You can adjust the width
-                img.style.margin = '10px'; // Add some margin between images
                 photoContainer.appendChild(img);
-            });
+            }
         })
         .catch((error) => console.error('Error fetching photos:', error));
-};
+}
+
+// Initial load (you can adjust the default page)
+loadPhotos(1);
+
+// Function to load a specific page
+function loadPage(page) {
+    loadPhotos(page);
+}
